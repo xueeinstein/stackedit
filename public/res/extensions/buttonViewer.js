6beb66e4b47ccd9278c471885653ab87385a6e1e
xueeinstein <xueeinstein@gmail.com>
2015-08-27 17:04:06 +0800

    init stackedit nw.js app

--- public/res/extensions/buttonViewer.js
+++ public/res/extensions/buttonViewer.js
@@ -2,13 +2,18 @@ define([
     "jquery",
     "classes/Extension",
     "text!html/buttonViewer.html",
-], function($, Extension, buttonViewerHTML) {
+    "text!html/buttonViewer_nw.html"
+], function($, Extension, buttonViewerHTML, buttonViewer_nw) {
 
     var buttonViewer = new Extension("buttonViewer", 'Button "Viewer"', true, true);
     buttonViewer.settingsBlock = '<p>Adds a "Viewer" button over the preview.</p>';
 
     buttonViewer.onCreatePreviewButton = function() {
-        return buttonViewerHTML;
+	    if(typeof window.nwDispatcher !== 'undefined') {
+	    	return buttonViewer_nw;
+	    } else {
+        	return buttonViewerHTML;
+	    }
     };
 
     return buttonViewer;
